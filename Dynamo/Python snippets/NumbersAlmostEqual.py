805524917c2e85e10e72439759a26d2c233af4c3
Konstantin <92411255+nodatasheet@users.noreply.github.com>
2022-04-03 18:56:50 +0800

    'main'

---
+++ Dynamo/Python snippets/NumbersAlmostEqual.py
@@ -0,0 +1,18 @@
+"""A function for testing approximate equality of two numbers.
+Same as math.isclose in Python v3.5 (and newer)
+https://www.python.org/dev/peps/pep-0485
+"""
+
+
+def almost_equal(a, b, rel_tol=1e-09, abs_tol=0.0):
+    """A function for testing approximate equality of two numbers.
+    Same as math.isclose in Python v3.5 (and newer)
+    https://www.python.org/dev/peps/pep-0485
+    """
+    return abs(a-b) <= max(rel_tol * max(abs(a), abs(b)), abs_tol)
+
+
+# Examples:
+print (almost_equal(5.2, 5.2))  # True
+print (almost_equal(5.201, 5.202))  # False
+print (almost_equal(5.201, 5.202, 0.1))  # True
