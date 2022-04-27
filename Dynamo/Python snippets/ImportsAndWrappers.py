2a2a38b435cf10f79e4f0dcc2e0cb1bd463f5abb
Konstantin <92411255+nodatasheet@users.noreply.github.com>
2022-04-27 17:50:19 +0800

    Added transactions with rollback

--- Dynamo/Python snippets/ImportsAndWrappers.py
+++ Dynamo/Python snippets/ImportsAndWrappers.py
@@ -1,5 +1,5 @@
 """
-
+Describe Imports and Conversions in Python code for Dynamo.
 https://github.com/DynamoDS/Dynamo/wiki/Python-0.6.3-to-0.7.x-Migration"""
 
 import clr
@@ -10,19 +10,44 @@ from System.Collections.Generic import List
 
 
 """ Document and Application """
-
-# Import DocumentManager and TransactionManager
 clr.AddReference("RevitServices")
+# Import DocumentManager 
 from RevitServices.Persistence import DocumentManager
-from RevitServices.Transactions import TransactionManager
-
 doc = DocumentManager.Instance.CurrentDBDocument
 uiapp = DocumentManager.Instance.CurrentUIApplication
 app = uiapp.Application
 
+
+""" Transaction Manager """
+# Import DocumentManager and TransactionManager
+from RevitServices.Transactions import TransactionManager
+
+# TransactionManager is a layer around the regular Revit.DB.Transaction.
+# It is preferable to use TransactionManager in Dynamo
+# instead of regular transaction.
+# To rollback your changes use regular transaction as a subtransaction
+# inside the TransactionManager.
+# More explanation:
+# https://forum.dynamobim.com/t/transactionmanager-vs-transaction/7305/2
+# Naming Transaction in TransactionManager is not supported.
+# https://forum.dynamobim.com/t/name-a-transaction/23990/15
+
 # Start Transaction
 TransactionManager.Instance.EnsureInTransaction(doc)
-# End Transaction
+# End Transaction - Preferable
+TransactionManager.Instance.TransactionTaskDone()
+# Force Close Transaction - If above doesn't work
+TransactionManager.ForceCloseTransaction()
+
+# Transaction with rollback
+TransactionManager.Instance.EnsureInTransaction(doc)
+sub_transaction = SubTransaction(doc)
+sub_transaction.Start()
+# some code here
+if 'result is ok':
+    sub_transaction.Commit()
+else:
+    sub_transaction.RollBack()
 TransactionManager.Instance.TransactionTaskDone()
 
 
@@ -40,12 +65,12 @@ clr.ImportExtensions(Revit.Elements)
 clr.ImportExtensions(Revit.GeometryConversion)
 
 
-# Unwrap from Dynamo to Revit
+# Unwrap (from Dynamo to Revit)
 
-# Elements
-DynamoElement = UnwrapElement(IN[0])
+#   Elements
+    DynamoElement = UnwrapElement(IN[0])
 
-# Geometry
+#   Geometry
     Point.ToRevitType()  # XYZ
     Vector.ToRevitType()  # XYZ
     Plane.ToPlane()  # Plane
@@ -60,12 +85,13 @@ DynamoElement = UnwrapElement(IN[0])
     BoundingBox.ToRevitType()  # BoundingBoxXYZ
 
 
-# Wrap from Revit to Dynamo
+# Wrap (from Revit to Dynamo)
 
-# Elements
+#   Elements
     Element.ToDSType(True)  # Not created in script, mark as Revit-owned
     Element.ToDSType(False)  # Created in script, mark as non-Revit-owned
-# Geometry
+
+#   Geometry
     XYZ.ToPoint()  # Point
     XYZ.ToVector()  # Vector
     Point.ToProtoType()  # Point
