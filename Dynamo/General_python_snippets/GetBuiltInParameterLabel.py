c69e899d8980205c6e6c1d3e6a5f54a1d9a1b0ac
Konstantin <92411255+nodatasheet@users.noreply.github.com>
2022-06-15 15:07:45 +0800

    update description

--- Dynamo/General_python_snippets/GetBuiltInParameterLabel.py
+++ Dynamo/General_python_snippets/GetBuiltInParameterLabel.py
@@ -7,7 +7,7 @@ from Autodesk.Revit.DB import BuiltInParameter, LabelUtils
 
 def get_built_in_parameter_label(bip_name):
     # type: (str) -> str
-    """Gets the user-visible name for a BuiltInParameter"""
+    """Gets the user-visible name for a BuiltInParameter Enumeration name"""
     if Enum.IsDefined(BuiltInParameter, bip_name):
         bip_param = Enum.Parse(BuiltInParameter, bip_name)
         bip_label = LabelUtils.GetLabelFor(bip_param)
