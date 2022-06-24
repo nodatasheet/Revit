8f35656e6901d9ec30fc69ee5bcb9e1eb29e85a2
Konstantin <92411255+nodatasheet@users.noreply.github.com>
2022-06-24 21:08:30 +0800

    renamed variable

--- Dynamo/General_python_snippets/NumbersChunkUpToMaxVal.py
+++ Dynamo/General_python_snippets/NumbersChunkUpToMaxVal.py
@@ -6,16 +6,16 @@ def chunk_upto_max(numbers, max_size):
     # type: (list, float) -> list
     """Takes a list of numbers and splits them
     into values not exceeding the maximum size"""
-    chunked_lengths = []
+    chunked_numbers = []
     for num in numbers:
         if num > max_size:
             denominator = math.floor(num / max_size)
             remainder = num - denominator * max_size
-            chunked_lengths.append(remainder)
-            chunked_lengths.extend(repeat(max_size, denominator))
+            chunked_numbers.append(remainder)
+            chunked_numbers.extend(repeat(max_size, denominator))
         else:
-            chunked_lengths.append(num)
-    return chunked_lengths
+            chunked_numbers.append(num)
+    return chunked_numbers
 
 
 chunked_lengths = chunk_upto_max([2, 5, 10], 7)  # -> [2, 5, 3, 7]
