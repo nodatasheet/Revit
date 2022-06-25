f1fdcd59cd6cb7cfb82eeee5afbf38b2cfa2346e
Konstantin <92411255+nodatasheet@users.noreply.github.com>
2022-06-25 17:58:49 +0800

    refactor and add checking for non-negative

--- Dynamo/General_python_snippets/NumbersChunkUpToMaxVal.py
+++ Dynamo/General_python_snippets/NumbersChunkUpToMaxVal.py
@@ -1,21 +1,19 @@
-from itertools import repeat
-import math
-
-
 def chunk_upto_max(numbers, max_size):
     # type: (list, float) -> list
-    """Takes a list of numbers and splits them
-    into values not exceeding the maximum size"""
+    """Takes a list of positive numbers and splits them
+    into chunks not exceeding the maximum size"""
+    assert max_size >= 0, "Max size should be positive"
     chunked_numbers = []
     for num in numbers:
+        assert num >= 0, "Numbers should be positive"
         if num > max_size:
-            denominator = math.floor(num / max_size)
-            remainder = num - denominator * max_size
+            multiplier = int(num / max_size)
+            remainder = num - multiplier * max_size
             chunked_numbers.append(remainder)
-            chunked_numbers.extend(repeat(max_size, denominator))
+            chunked_numbers.extend(max_size for i in range(multiplier))
         else:
             chunked_numbers.append(num)
     return chunked_numbers
 
 
-chunked_lengths = chunk_upto_max([2, 5, 10], 7)  # -> [2, 5, 3, 7]
+chunk_upto_max([2, 5, 10], 7)  # -> [2, 5, 3, 7]
