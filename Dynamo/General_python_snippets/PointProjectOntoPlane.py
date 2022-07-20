077e3742c3035a50156fc0798d2c00ff0ecc7b5a
Konstantin <92411255+nodatasheet@users.noreply.github.com>
2022-07-20 18:51:39 +0800

    Project a 3D point orthogonally onto a Plane

---
+++ Dynamo/General_python_snippets/PointProjectOntoPlane.py
@@ -0,0 +1,14 @@
+"""Project a 3D point orthogonally onto a Plane.
+Starting Revit 2018.1 can use Plane.Project() method instead."""
+
+import clr
+clr.AddReference("RevitAPI")
+from Autodesk.Revit.DB import XYZ, Plane, Point
+
+
+def point_project_onto_plane(point, plane):
+    # type: (XYZ, Plane) -> Point
+    normal = plane.Normal
+    vec_to_orig = point.Subtract(plane.Origin)
+    signed_dist = normal.DotProduct(vec_to_orig)
+    return point.Subtract(normal.Multiply(signed_dist))
