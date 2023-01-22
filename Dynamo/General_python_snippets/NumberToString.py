a26a58d0e86622f1f22f15def557a35c6f289e78
Konstantin <92411255+nodatasheet@users.noreply.github.com>
2023-01-22 19:18:00 +0800

    add support System.Int64

--- Dynamo/General_python_snippets/NumberToString.py
+++ Dynamo/General_python_snippets/NumberToString.py
@@ -6,11 +6,12 @@ https://stackoverflow.com/a/37736333
 
 from numbers import Number
 from System.Globalization import CultureInfo
+from System import Int64
 
 
 def tolist(obj):
     # type: (object) -> list
-    """Converts object to list if it is not a list"""
+    """Converts an object to a list if it is not a list"""
     if isinstance(obj, list):
         return obj
     return [obj]
@@ -18,18 +19,18 @@ def tolist(obj):
 
 def number_to_string(num):
     # type: (Number) -> str
-    """Converts number to string without trailing zeros"""
+    """Converts a number to a string without trailing zeros"""
     s = str(num)
     return '0' if s == '0' else s.rstrip('0').rstrip('.')
 
 
-def validate_type(obj, expected_type):
-    # type: (object, type) -> None
-    """Validates that object is of expected type"""
-    if not isinstance(obj, expected_type):
+def validate_number(obj):
+    # type: (object) -> None
+    """Validates that object is a Number"""
+    # in Dynamo int is System.Int64 which is not recognized as a Number
+    if not isinstance(obj, (Number, Int64)):
         raise TypeError(
-            'Expected <{}>, got <{}>'.format(expected_type.__name__,
-                                             type(obj).__name__)
+            'Expected <Number>, got <{}>'.format(type(obj).__name__)
         )
 
 
@@ -42,7 +43,7 @@ decimal_separator = \
 
 output_strings = []
 for num in input_numbers:
-    validate_type(num, Number)
+    validate_number(num)
     string_from_number = number_to_string(num)
     localized_string = string_from_number.replace('.', decimal_separator)
     output_strings.append(localized_string)
